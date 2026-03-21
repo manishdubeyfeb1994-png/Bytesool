cc2d0651af68208a5f41d7feec0f9f2a2107860f
manishdubeyfeb1994-png <manishdubeyfeb1994@gmail.com>
2026-03-21 18:28:24 +0530

    Implement routing and replace maintenance message

--- bytesool/src/App.jsx
+++ bytesool/src/App.jsx
@@ -1,16 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/HomePage";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div style={{
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-      fontFamily: "Arial"
-    }}>
-      <h1>🚧 Website Temporarily Stopped</h1>
-      <p>We are doing maintenance. Please visit again later.</p>
-    </div>
+    <Router>
+      <Navbar /> {/* Always visible */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer /> {/* Always visible */}
+    </Router>
   );
 }
 
